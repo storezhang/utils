54e521b5f277373cbc6bfbe86b18d1bbe8321773
storezhang <storezhang@gmail.com>
2016-12-07 10:05:39 +0800

    1、测试JSONP；

--- travis/deploy-to-sonatype.sh
+++ travis/deploy-to-sonatype.sh
@@ -2,6 +2,6 @@
 
 # 部署到Maven中央仓库
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  mvn clean deploy sonatype-oss-release --settings travis/settings.xml
+  mvn clean deploy --settings travis/settings.xml
   echo -e "成功部署${TRAVIS_JOB_NUMBER}到Maven中央仓库！"
 fi
