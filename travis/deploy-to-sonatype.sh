92f6e205070613cd1bb1e6b05336a21349de6aa2
storezhang <storezhang@gmail.com>
2016-12-07 11:43:45 +0800

    1、测试JSONP；

--- travis/deploy-to-sonatype.sh
+++ travis/deploy-to-sonatype.sh
@@ -9,9 +9,16 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
         rm -rf ~/.gnupg
     fi
     # 生成GPG
-    source ./create_gpg.sh
+    source .travis/create_gpg.sh
     echo -e "成功生成GPG短语${evn.GPG_PASSPHRASE}！\n"
 
     mvn clean deploy -Psonatype-oss-release -DskipTests=true -Dgpg.skip=true --settings travis/settings.xml -Dgpg.passphrase=${env.GPG_PASSPHRASE}
+
+    # 清理GPG
+    if [ ! -z "$TRAVIS" ]; then
+        shred -v ~/.gnupg/*
+        rm -rf ~/.gnupg
+    fi
+
     echo -e "成功部署${TRAVIS_JOB_NUMBER}到Maven中央仓库！\n"
 fi
