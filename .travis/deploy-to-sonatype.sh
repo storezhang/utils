64ab529d718138f99280a9491da28407bafecdc8
storezhang <storezhang@gmail.com>
2016-12-09 13:07:43 +0800

    1、测试JSONP；

--- .travis/deploy-to-sonatype.sh
+++ .travis/deploy-to-sonatype.sh
@@ -4,7 +4,7 @@ set -e
 
 # 部署到Maven中央仓库
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
+    mvn --settings .travis/settings.xml -Psonatype-oss-release
 
     # 清理GPG
     if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
@@ -14,7 +14,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
 
     source .travis/create_gpg.sh
 
-    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Psonatype-oss-release
 
     # 清理GPG
     if [ ! -z "$TRAVIS" ]; then
