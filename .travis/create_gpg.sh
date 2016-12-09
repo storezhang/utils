b0ca251cf9a4c8dedb66691fef403e8875f4281d
storezhang <storezhang@gmail.com>
2016-12-09 14:56:55 +0800

    1、测试发布到Maven中央仓库；

--- .travis/create_gpg.sh
+++ .travis/create_gpg.sh
@@ -7,7 +7,7 @@ export GPG_PASSPHRASE=$(echo "$RANDOM$(date)" | md5sum | cut -d\  -f1)
 
 # 配置生成GPG密钥
 cat >gen-key-script <<EOF
-    %echo 开始生成GPG
+    %echo -e 开始生成GPG
     Key-Type: RSA
     Key-Length: 2048
     Subkey-Type: 1
@@ -17,7 +17,7 @@ cat >gen-key-script <<EOF
     Expire-Date: 0
     Passphrase: ${GPG_PASSPHRASE}
     %commit
-    %echo 生成GPG成功
+    %echo -e 生成GPG成功
 EOF
 
 # 创建GPG对
@@ -25,16 +25,17 @@ gpg --batch --gen-key gen-key-script
 
 
 # 导出GPG对
-export GPG_KEYNAME=$(gpg -K | grep ^sec | cut -d/  -f2 | cut -d\  -f1 | head -n1)
+export GPG_KEYNAME = $(gpg -K | grep ^sec | cut -d/  -f2 | cut -d\  -f1 | head -n1)
+echo -e "生成的GPG是：键（${GPG_KEYNAME}：${GPG_PASSPHRASE}）"
 
 # 清除本地配置
 shred gen-key-script
 
 # 将本地GPG发布到Ubuntu的服务器上
-gpg --keyserver keyserver.ubuntu.com --send-keys ${GPG_KEYNAME}
+gpg --keyserver hkp://keyserver.ubuntu.com:11371 --send-keys ${GPG_KEYNAME}
 
 # 等待刚发布的GPG可用
 while (true); do
   date
-  gpg --keyserver keyserver.ubuntu.com  --recv-keys ${GPG_KEYNAME} && break || sleep 30
+  gpg --keyserver hkp://keyserver.ubuntu.com:11371 --recv-keys ${GPG_KEYNAME} && break || sleep 30
 done
