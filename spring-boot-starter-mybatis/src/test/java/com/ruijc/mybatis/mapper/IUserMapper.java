24f3c387c48984d0f3f37eac1f807ff9cda7ae4d
storezhang <storezhang@gmail.com>
2016-12-29 03:03:46 +0800

    1、增加Operation；

--- spring-boot-starter-mybatis/src/test/java/com/ruijc/mybatis/mapper/IUserMapper.java
+++ spring-boot-starter-mybatis/src/test/java/com/ruijc/mybatis/mapper/IUserMapper.java
@@ -2,10 +2,12 @@ package com.ruijc.mybatis.mapper;
 
 import com.ruijc.mybatis.BaseMapper;
 import com.ruijc.mybatis.bean.User;
+import com.ruijc.mybatis.cache.redis.LoggingRedisCache;
+import com.ruijc.mybatis.cache.redis.RedisCache;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface IUserMapper extends BaseMapper<User> {
-
     int countAll();
 }
